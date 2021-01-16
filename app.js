85db8f73937c236ba4f4b2bc0c08311f79a1cfca
sumanpatra-123 <64701665+sumanpatra-123@users.noreply.github.com>
2021-01-16 13:38:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect("Add ur mongodbWebsite", {useNewUrlParser:true, useUnifiedTopology: true});
 
 const postSchema = {
   title: String,
